18fc91fcb5700a878b0bf89fdf379875d12b0b8c
Bret Shroats <bshroats5@gmail.com>
2024-02-01 21:04:49 -0500

    updates

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,5 +1,11 @@
-SELECT id AS Movie_ID, name AS Movie_Title, imdb_rating AS Rating
-FROM movies
-WHERE genre = 'Horror' AND year <= 1985
-ORDER BY imdb_rating DESC
-LIMIT 3;
+SELECT
+    id AS Movie_ID,
+    name AS Movie_Title,
+    imdb_rating AS Rating
+FROM
+    movies
+WHERE
+    genre = 'horror'
+    AND year <= 1985
+ORDER BY
+    imdb_rating DESC
